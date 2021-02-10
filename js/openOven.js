55bb96bd2add16658b654e89d5e3f7e06ccd9e32
illfriday <willprouty@gmail.com>
2021-02-10 15:20:46 -0800

    add card collapse add card content

---
+++ js/openOven.js
@@ -0,0 +1,75 @@
+//  Stops carousel from auto - scrolling
+$(function() {
+  $('.cardCarousel').carousel({
+    pause: true,
+    interval: false
+  });
+})
+
+
+// stops contestCarousel from auto scrolling
+$(function() {
+  $('#contestCarousel').carousel({
+    interval: false,
+  });
+  $('#carousel-thumbs').carousel({
+    interval: false
+  });
+})
+// handles the carousel thumbnails
+// https://stackoverflow.com/questions/25752187/bootstrap-carousel-with-thumbnails-multiple-carousel
+
+$(function () {
+  $('[id^=carousel-selector-]').click(function () {
+    var id_selector = $(this).attr('id');
+    var id = parseInt(id_selector.substr(id_selector.lastIndexOf('-') + 1));
+    $('#contestCarousel').carousel(id);
+  });
+})
+// Only display 3 items in nav on mobile.
+
+$(window).resize(function () {
+  if ($(window).width() < 575) {
+    $('#carousel-thumbs .row div:nth-child(4)').each(function () {
+      var rowBoundary = $(this);
+      $('<div class="row mx-0">').insertAfter(rowBoundary.parent()).append(rowBoundary.nextAll().addBack());
+    });
+    $('#carousel-thumbs .carousel-item .row:nth-child(even)').each(function () {
+      var boundary = $(this);
+      $('<div class="carousel-item">').insertAfter(boundary.parent()).append(boundary.nextAll().addBack());
+  });
+  }
+})  
+// Hide slide arrows if too few items.
+
+$(function () {
+  if ($('#carousel-thumbs .carousel-item').length < 2) {
+    $('#carousel-thumbs [class^=carousel-control-]').remove();
+    $('.machine-carousel-container #carousel-thumbs').css('padding', '0 5px');
+  }
+})
+
+// when the carousel slides, auto update
+$(function () {
+  $('#contestCarousel').on('slide.bs.carousel', function (e) {
+    var id = parseInt($(e.relatedTarget).attr('data-slide-number'));
+    $('[id^=carousel-selector-]').removeClass('selected');
+    $('[id=carousel-selector-' + id + ']').addClass('selected');
+  });
+})
+
+// when user swipes, go next or previous
+$(function () {
+  $('#contestCarousel').swipe({
+    fallbackToMouseEvents: true,
+    swipeLeft: function (e) {
+      $('#contestCarousel').carousel('next');
+    },
+    swipeRight: function (e) {
+      $('#contestCarousel').carousel('prev');
+    },
+    allowPageScroll: 'vertical',
+    preventDefaultEvents: false,
+    threshold: 75
+  });
+})
